1f26de7e9a0648bf2d03d0a97230940ac8ee124e
Nikolay Utkin <utkin.kolya2012@yandex.ru>
2024-03-06 15:08:31 +0300

    Переместил активити в слой Presentation

--- presentation/src/main/java/com/blackhito/presentation/activity/MainActivity.kt
+++ presentation/src/main/java/com/blackhito/presentation/activity/MainActivity.kt
@@ -1,11 +1,14 @@
-package com.blackhito.presentation
+package com.blackhito.presentation.activity
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.blackhito.presentation.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
