8ef3627c561cdb2a5f8a1990201b85ec43aa4a55
Nikolay Utkin <utkin.kolya2012@yandex.ru>
2024-03-06 15:06:27 +0300

    Добавил в манифест разрешение для доступа в интернет и app name

---
+++ app/src/main/java/com/blackhito/currencyexchange/di/modules/CurrencyExchangeApp.kt
@@ -0,0 +1,7 @@
+package com.blackhito.currencyexchange.di.modules
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class CurrencyExchangeApp: Application()
